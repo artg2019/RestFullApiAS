13ab2fa825be15ac5c62d09898b14c10605ff9aa
Artem Gorlov <artem0gorlov@gmail.com>
2022-05-27 00:19:16 +0300

    Add project files.

---
+++ Rest.Entities/Competention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest.Entities
+{
+    public class Competention
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+        public List<Competention> Competentions { get; set; }
+        public List<Participant> Participants { get; set; }
+        public List<Team> Teams { get; set; }
+    }
+}
---
+++ Rest.Entities/Participant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest.Entities
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public int CompetentionId { get; set; }
+        public Competention Competention { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
